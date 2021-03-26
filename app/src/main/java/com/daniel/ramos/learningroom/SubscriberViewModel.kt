8b65f96d2a29f7d50cc0d47cdf6ab9102146c9be
Daniel Ramos <danielmfr6@gmail.com>
2021-03-26 16:31:59 -0300

    Aplicar validação nos campos de input

--- app/src/main/java/com/daniel/ramos/learningroom/SubscriberViewModel.kt
+++ app/src/main/java/com/daniel/ramos/learningroom/SubscriberViewModel.kt
@@ -1,5 +1,6 @@
 package com.daniel.ramos.learningroom
 
+import android.util.Patterns
 import androidx.databinding.Bindable
 import androidx.databinding.Observable
 import androidx.lifecycle.LiveData
@@ -40,16 +41,26 @@ class SubscriberViewModel(private val repository: SubscriberRepository) : ViewMo
     }
 
     fun saveOrUpdate() {
-        if (isUpdateOrDelete) {
-            subscriberToUpdateOrDelete.name = inputName.value!!
-            subscriberToUpdateOrDelete.email = inputEmail.value!!
-            update(subscriberToUpdateOrDelete)
-        } else {
-            val name = inputName.value!!
-            val email = inputEmail.value!!
-            insert(Subscriber(0, name, email))
-            inputName.value = null
-            inputEmail.value = null
+
+        if (inputName.value.isNullOrEmpty())
+            statusMessage.value = Event("Please enter subscriber's name")
+        else if (inputEmail.value.isNullOrEmpty())
+            statusMessage.value = Event("Please enter subscriber's email")
+        else if (!Patterns.EMAIL_ADDRESS.matcher(inputEmail.value!!).matches())
+            statusMessage.value = Event("Please enter a correct email address")
+        else
+        {
+            if (isUpdateOrDelete) {
+                subscriberToUpdateOrDelete.name = inputName.value!!
+                subscriberToUpdateOrDelete.email = inputEmail.value!!
+                update(subscriberToUpdateOrDelete)
+            } else {
+                val name = inputName.value!!
+                val email = inputEmail.value!!
+                insert(Subscriber(0, name, email))
+                inputName.value = null
+                inputEmail.value = null
+            }
         }
     }
 
