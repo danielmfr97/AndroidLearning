4d1b6d3b513a5b39b3508cfc0c3398ed42833c71
Daniel Ramos <danielmfr6@gmail.com>
2021-06-09 14:33:12 -0300

    Fix

---
+++ LearningDagger/app/src/main/java/com/devventure/learningdagger/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.devventure.learningdagger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import javax.inject.Inject
+
+class MainActivity : AppCompatActivity() {
+    @Inject lateinit var smartPhone: SmartPhone
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        (application as SmartPhoneApplication).smartPhoneComponent
+            .inject(this)
+
+//        val smartPhone = SmartPhone(
+//            Battery(),
+//            SIMCard(ServiceProvider()),
+//            MemoryCard()
+//        )
+//            .makeACallWithRecording()
+
+        // Dado que um de nossos módulos tem um state, não podemos mais usar o create()
+//        DaggerSmartPhoneComponent.create().inject(this)
+//        smartPhone.makeACallWithRecording()
+
+//        DaggerSmartPhoneComponent.builder()
+//            .memoryCardModule(MemoryCardModule(1000))
+//            .build()
+//            .inject(this)
+    }
+}
