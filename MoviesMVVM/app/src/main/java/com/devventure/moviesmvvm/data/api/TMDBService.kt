68ba83c3353af30f2a5cf09056a4c5b989ee7224
Daniel Ramos <danielmfr6@gmail.com>
2021-06-10 09:48:00 -0300

    Criar interface para buscar dados

---
+++ MoviesMVVM/app/src/main/java/com/devventure/moviesmvvm/data/api/TMDBService.kt
@@ -0,0 +1,23 @@
+package com.devventure.moviesmvvm.data.api
+
+import com.devventure.moviesmvvm.data.ArtistList
+import com.devventure.moviesmvvm.data.MovieList
+import com.devventure.moviesmvvm.data.TvShowList
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * Created by danie on 10/06/2021
+ */
+interface TMDBService {
+    // Dado que vamos usar coroutines para processamento em background, declaramos suspending
+    @GET("movie/popular")
+    suspend fun getPopularMovies(@Query("api_key") apiKey: String): Response<MovieList>
+
+    @GET("tv/popular")
+    suspend fun getPopularTvShows(@Query("api_key") apiKey: String): Response<TvShowList>
+
+    @GET("person/popular")
+    suspend fun getPopularArtist(@Query("api_key") apiKey: String): Response<ArtistList>
+}
