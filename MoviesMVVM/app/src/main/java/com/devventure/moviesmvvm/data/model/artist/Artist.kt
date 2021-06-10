98ba6dc8067db96f6e5a2e5d06f7673a3882b683
Daniel Ramos <danielmfr6@gmail.com>
2021-06-10 09:52:38 -0300

    Remover classe KnownFor.kt

--- MoviesMVVM/app/src/main/java/com/devventure/moviesmvvm/data/model/artist/Artist.kt
+++ MoviesMVVM/app/src/main/java/com/devventure/moviesmvvm/data/model/artist/Artist.kt
@@ -2,7 +2,6 @@ package com.devventure.moviesmvvm.data.model.artist
 
 data class Artist(
     val id: Int,
-    val known_for: List<KnownFor>,
     val name: String,
     val popularity: Double,
     val profile_path: String
