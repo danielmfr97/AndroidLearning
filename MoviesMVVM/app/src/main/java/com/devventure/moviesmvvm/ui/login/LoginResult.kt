8608471374fc534a9089132d7b9e2acb66f68ff9
Daniel Farias <danielmfr6@gmail.com>
2021-07-12 18:49:57 -0300

    Ajustes de formatação
    Ajustes

---
+++ MoviesMVVM/app/src/main/java/com/devventure/moviesmvvm/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.devventure.moviesmvvm.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
