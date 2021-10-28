b15705e0dc1984e946491d00024fa45a5f124946
Daniel Farias <danielmfr6@gmail.com>
2021-10-28 20:00:59 -0300

    Update codelabs testing kotlin

---
+++ Codelabs/android_testing_code_01/app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/FakeDataSource.kt
@@ -0,0 +1,79 @@
+package com.example.android.architecture.blueprints.todoapp.data.source
+
+import androidx.lifecycle.LiveData
+import com.example.android.architecture.blueprints.todoapp.data.Result
+import com.example.android.architecture.blueprints.todoapp.data.Result.Error
+import com.example.android.architecture.blueprints.todoapp.data.Result.Success
+import com.example.android.architecture.blueprints.todoapp.data.Task
+
+/**
+ * FakeDataSource is a specific type of test double called a fake.
+ * A fake is a test double that has a "working" implementation of the class,
+ * but it's implemented in a way that makes it good for tests but unsuitable for production.
+ * "Working" implementation means that the class will produce realistic outputs given inputs.
+
+ * For example, your fake data source won't connect to the network or save anything to a database—instead
+ * it will just use an in-memory list. This will "work as you might expect" in that methods to get or save
+ * tasks will return expected results, but you could never use this implementation in production,
+ * because it's not saved to the server or a database.
+ */
+class FakeDataSource(var tasks: MutableList<Task>? = mutableListOf()) : TasksDataSource{
+    override fun observeTasks(): LiveData<Result<List<Task>>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTasks(): Result<List<Task>> {
+        tasks?.let { return Success(ArrayList(it)) }
+        return Error(
+                Exception("Tasks not found")
+        )
+    }
+
+    override suspend fun refreshTasks() {
+        TODO("Not yet implemented")
+    }
+
+    override fun observeTask(taskId: String): LiveData<Result<Task>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTask(taskId: String): Result<Task> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun refreshTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun saveTask(task: Task) {
+        tasks?.add(task)
+    }
+
+    override suspend fun completeTask(task: Task) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun completeTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun activateTask(task: Task) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun activateTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun clearCompletedTasks() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteAllTasks() {
+        tasks?.clear()
+    }
+
+    override suspend fun deleteTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ Codelabs/android_testing_code_01/app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/FakeTestRepository.kt
@@ -0,0 +1,92 @@
+package com.example.android.architecture.blueprints.todoapp.data.source
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.android.architecture.blueprints.todoapp.data.Result
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import kotlinx.coroutines.runBlocking
+
+/**
+ * Note that your FakeTestRepository doesn't need to use FakeDataSources or anything like that;
+ * it just needs to return realistic fake outputs given inputs.
+ * You will use a LinkedHashMap to store the list of tasks and a MutableLiveData for your observable tasks.
+ */
+class FakeTestRepository : TasksRepository {
+    var tasksServiceData: LinkedHashMap<String, Task> = LinkedHashMap()
+
+    private val observableTasks = MutableLiveData<Result<List<Task>>>()
+
+    /**
+     * Method for adding new tasks easily
+     */
+    fun addTasks(vararg tasks: Task) {
+        for (task in tasks) {
+            tasksServiceData[task.id] = task
+        }
+        runBlocking { refreshTasks() }
+    }
+
+    override suspend fun getTasks(forceUpdate: Boolean): Result<List<Task>> {
+        return Result.Success(tasksServiceData.values.toList())
+    }
+
+    override suspend fun refreshTasks() {
+        observableTasks.value = getTasks()
+    }
+
+    override fun observeTasks(): LiveData<Result<List<Task>>> {
+        /**
+         * For your test double, use runBlocking. runBlocking,
+         * which is a closer simulation of what a "real" implementation of the repository would do,
+         * and it's preferable for Fakes, so that their behavior more closely matches the real implementation.
+         */
+        runBlocking { refreshTasks() }
+        return observableTasks
+    }
+
+    override suspend fun refreshTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun observeTask(taskId: String): LiveData<Result<Task>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTask(taskId: String, forceUpdate: Boolean): Result<Task> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun saveTask(task: Task) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun completeTask(task: Task) {
+        val completedTask = task.copy(isCompleted = true)
+        tasksServiceData[task.id] = completedTask
+        refreshTasks()
+    }
+
+    override suspend fun completeTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun activateTask(task: Task) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun activateTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun clearCompletedTasks() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteAllTasks() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+}
