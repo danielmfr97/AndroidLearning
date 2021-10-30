8f3f95be29e6204e5141a0a64f9984e129f61838
Daniel Farias <danielmfr6@gmail.com>
2021-10-30 09:21:35 -0300

    Remove funs from inside sealed class

--- CWM/DotaInfo/hero/hero-domain/src/main/java/br/com/daniel/ramos/hero_domain/HeroAttribute.kt
+++ CWM/DotaInfo/hero/hero-domain/src/main/java/br/com/daniel/ramos/hero_domain/HeroAttribute.kt
@@ -22,68 +22,69 @@ sealed class HeroAttribute(
         uiValue = "Unknown",
         abbreviation = "unknown"
     )
+}
 
-    fun getHeroAttrFromUiValue(uiValue: String): HeroAttribute {
-        return when (uiValue) {
-            HeroAttribute.Agility.uiValue -> {
-                HeroAttribute.Agility
-            }
-            HeroAttribute.Strength.uiValue -> {
-                HeroAttribute.Strength
-            }
-            HeroAttribute.Intelligence.uiValue -> {
-                HeroAttribute.Intelligence
-            }
-            else -> HeroAttribute.Unknown
+
+fun getHeroAttrFromUiValue(uiValue: String): HeroAttribute {
+    return when (uiValue) {
+        HeroAttribute.Agility.uiValue -> {
+            HeroAttribute.Agility
+        }
+        HeroAttribute.Strength.uiValue -> {
+            HeroAttribute.Strength
+        }
+        HeroAttribute.Intelligence.uiValue -> {
+            HeroAttribute.Intelligence
+        }
+        else -> HeroAttribute.Unknown
+    }
+}
+
+fun getHeroAttrFromAbbreviation(abbreviation: String): HeroAttribute {
+    return when (abbreviation) {
+        HeroAttribute.Agility.abbreviation -> {
+            HeroAttribute.Agility
+        }
+        HeroAttribute.Strength.abbreviation -> {
+            HeroAttribute.Strength
+        }
+        HeroAttribute.Intelligence.abbreviation -> {
+            HeroAttribute.Intelligence
+        }
+        else -> HeroAttribute.Unknown
+    }
+}
+
+fun Hero.minAttackDmg(): Int {
+    return when (primaryAttribute) {
+        is HeroAttribute.Strength -> {
+            baseAttackMin + baseStr
+        }
+        is HeroAttribute.Agility -> {
+            baseAttackMin + baseAgi
+        }
+        is HeroAttribute.Intelligence -> {
+            baseAttackMin + baseInt
+        }
+        is HeroAttribute.Unknown -> {
+            0
         }
     }
+}
 
-    fun getHeroAttrFromAbbreviation(abbreviation: String): HeroAttribute {
-        return when (abbreviation) {
-            HeroAttribute.Agility.abbreviation -> {
-                HeroAttribute.Agility
-            }
-            HeroAttribute.Strength.abbreviation -> {
-                HeroAttribute.Strength
-            }
-            HeroAttribute.Intelligence.abbreviation -> {
-                HeroAttribute.Intelligence
-            }
-            else -> HeroAttribute.Unknown
+fun Hero.maxAttackDmg(): Int {
+    return when (primaryAttribute) {
+        is HeroAttribute.Strength -> {
+            baseAttackMax + baseStr
         }
-    }
-
-    fun Hero.minAttackDmg(): Int {
-        return when (primaryAttribute) {
-            is HeroAttribute.Strength -> {
-                baseAttackMin + baseStr
-            }
-            is HeroAttribute.Agility -> {
-                baseAttackMin + baseAgi
-            }
-            is HeroAttribute.Intelligence -> {
-                baseAttackMin + baseInt
-            }
-            is HeroAttribute.Unknown -> {
-                0
-            }
+        is HeroAttribute.Agility -> {
+            baseAttackMax + baseAgi
         }
-    }
-
-    fun Hero.maxAttackDmg(): Int {
-        return when (primaryAttribute) {
-            is HeroAttribute.Strength -> {
-                baseAttackMax + baseStr
-            }
-            is HeroAttribute.Agility -> {
-                baseAttackMax + baseAgi
-            }
-            is HeroAttribute.Intelligence -> {
-                baseAttackMax + baseInt
-            }
-            is HeroAttribute.Unknown -> {
-                0
-            }
+        is HeroAttribute.Intelligence -> {
+            baseAttackMax + baseInt
+        }
+        is HeroAttribute.Unknown -> {
+            0
         }
     }
 }
