7ea65c461d2d6d7024e2b9253461ef66da8fe1c2
Daniel Farias <danielmfr6@gmail.com>
2021-11-04 07:33:11 -0300

    Testing GetHeroes Use-case
    - Testing usecase in mainactivity using compose
    - Adding uses permission for internet

--- CWM/DotaInfo/app/src/main/java/br/com/daniel/ramos/dotainfo/MainActivity.kt
+++ CWM/DotaInfo/app/src/main/java/br/com/daniel/ramos/dotainfo/MainActivity.kt
@@ -3,21 +3,75 @@ package br.com.daniel.ramos.dotainfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import br.com.daniel.ramos.core.DataState
+import br.com.daniel.ramos.core.Logger
+import br.com.daniel.ramos.core.ProgressBarState
+import br.com.daniel.ramos.core.UIComponent
 import br.com.daniel.ramos.dotainfo.ui.theme.DotaInfoTheme
+import br.com.daniel.ramos.hero_domain.Hero
+import br.com.daniel.ramos.hero_interactors.HeroInteractors
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class MainActivity : ComponentActivity() {
+
+    private val heros: MutableState<List<Hero>> = mutableStateOf(listOf())
+    private val progressBarState: MutableState<ProgressBarState> =
+        mutableStateOf(ProgressBarState.Idle)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val getHeroes = HeroInteractors.build().getHeroes
+        val logger = Logger("GetHerosTest")
+        getHeroes.execute().onEach { dataState ->
+            when (dataState) {
+                is DataState.Response -> {
+                    when (dataState.uiComponent) {
+                        is UIComponent.Dialog -> {
+                            logger.log((dataState.uiComponent as UIComponent.Dialog).description)
+                        }
+                        is UIComponent.None -> {
+                            logger.log((dataState.uiComponent as UIComponent.None).message)
+                        }
+                    }
+                }
+                is DataState.Data -> {
+                    heros.value = dataState.data ?: listOf()
+                }
+                is DataState.Loading -> {
+                    progressBarState.value = dataState.progressBarState
+                }
+            }
+        }.launchIn(CoroutineScope(IO))
+
         setContent {
             DotaInfoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Box(modifier = Modifier.fillMaxSize()) {
+                    LazyColumn {
+                        items(heros.value) { hero ->
+                            Text(hero.localizedName)
+                        }
+                    }
+                    if (progressBarState.value is ProgressBarState.Loading) {
+                        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                    }
                 }
             }
         }
