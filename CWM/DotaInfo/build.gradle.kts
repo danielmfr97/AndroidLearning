46342dc48caa55e2d82806a64313e2e9be30d3c6
Daniel Farias <danielmfr6@gmail.com>
2021-11-04 07:09:25 -0300

    Remove kotlin classpath

--- CWM/DotaInfo/build.gradle.kts
+++ CWM/DotaInfo/build.gradle.kts
@@ -7,7 +7,6 @@ buildscript {
     dependencies {
         classpath(Build.androidBuildTools)
         classpath(Build.kotlinGradlePlugin)
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
     }
 }
 
