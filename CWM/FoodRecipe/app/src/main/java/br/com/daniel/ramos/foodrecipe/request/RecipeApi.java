10549873f17d702daa7ff77850e1f247ed939245
Daniel Farias <danielmfr6@gmail.com>
2021-11-08 21:42:08 -0300

    Retrofit Interface Methods

--- CWM/FoodRecipe/app/src/main/java/br/com/daniel/ramos/foodrecipe/request/RecipeApi.java
+++ CWM/FoodRecipe/app/src/main/java/br/com/daniel/ramos/foodrecipe/request/RecipeApi.java
@@ -1,8 +1,26 @@
 package br.com.daniel.ramos.foodrecipe.request;
 
+import br.com.daniel.ramos.foodrecipe.request.responses.RecipeResponse;
+import br.com.daniel.ramos.foodrecipe.request.responses.RecipeSearchResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 /**
  * Onde teremos todas as requests da API
  */
 public interface RecipeApi {
 
+    // SEARCH
+    @GET("api/v2/recipes")
+    Call<RecipeSearchResponse> searchRecipe(
+            @Query("q") String query,
+            @Query("page") String page
+    );
+
+    // GET RECIPE
+    @GET("api/v2/get")
+    Call<RecipeResponse> getRecipe(
+            @Query("rId") String recipe_id
+    );
 }
