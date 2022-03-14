7249a970c35b9c7e239878b23c650d42265b2cde
Daniel Ramos <danielmfr6@gmail.com>
2022-03-14 15:09:02 -0300

    Using navigation components with Compose

--- CWM/LearningJetpackCompose/app/src/main/java/br/com/daniel/ramos/learningjetpackcompose/MainActivity.kt
+++ CWM/LearningJetpackCompose/app/src/main/java/br/com/daniel/ramos/learningjetpackcompose/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction().replace(R.id.main_container, RecipeListFragment())
+        supportFragmentManager.beginTransaction().replace(R.id.main_nav_host_fragment, RecipeListFragment())
             .commit()
     }
 
--- CWM/LearningJetpackCompose/app/src/main/java/br/com/daniel/ramos/learningjetpackcompose/RecipeListFragment.kt
+++ CWM/LearningJetpackCompose/app/src/main/java/br/com/daniel/ramos/learningjetpackcompose/RecipeListFragment.kt
@@ -4,41 +4,43 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 
 class RecipeListFragment : Fragment() {
+    @ExperimentalUnitApi
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        val view = inflater.inflate(R.layout.fragment_recipe_list, container, false)
-        view.findViewById<ComposeView>(R.id.compose_view).setContent {
-            Column(modifier = Modifier.border(border = BorderStroke(1.dp, Color.Black))) {
-                Text("This is a composable inside the fragment")
-                Spacer(modifier = Modifier.padding(10.dp))
-                CircularProgressIndicator()
-                Spacer(modifier = Modifier.padding(10.dp))
-                Text("NEAT")
-                Spacer(modifier = Modifier.padding(10.dp))
-                val customView = HorizontalProgressBar(LocalContext.current)
-                AndroidView(factory = {customView})
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(
+                        text = "Recipe List",
+                        style = TextStyle(
+                            fontSize = TextUnit(21F, TextUnitType.Sp)
+                        )
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Button(onClick = { findNavController().navigate(R.id.viewRecipe) }) {
+                        Text("TO RECIPE FRAGMENT")
+                    }
+                }
             }
         }
-        return view
     }
 }
