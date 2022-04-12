9b739c0f9988b5e53fba51840637fb9f864e95e3
Daniel Ramos <danielmfr6@gmail.com>
2022-04-12 08:34:04 -0300

    Create Chip items in toolbar

--- CWM/LearningJetpackCompose/app/src/main/java/br/com/daniel/ramos/learningjetpackcompose/presentation/ui/recipe_list/RecipeListFragment.kt
+++ CWM/LearningJetpackCompose/app/src/main/java/br/com/daniel/ramos/learningjetpackcompose/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -14,9 +16,9 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -24,6 +26,7 @@ import androidx.compose.ui.unit.ExperimentalUnitApi
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import br.com.daniel.ramos.learningjetpackcompose.presentation.components.FoodCategoryChip
 import br.com.daniel.ramos.learningjetpackcompose.presentation.components.RecipeCard
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -52,9 +55,10 @@ class RecipeListFragment : Fragment() {
                 Column {
                     Surface(
                         modifier = Modifier.fillMaxWidth(),
-                        color = MaterialTheme.colors.primary,
+                        color = Color.White,
                         elevation = 8.dp
                     ) {
+                        Column() {
                             Row(modifier = Modifier.fillMaxWidth()) {
                                 TextField(
                                     modifier = Modifier
@@ -83,6 +87,18 @@ class RecipeListFragment : Fragment() {
                                     colors = TextFieldDefaults.textFieldColors(backgroundColor = MaterialTheme.colors.surface)
                                 )
                             }
+                            LazyRow(content = {
+                                items(getAllFoodCategories()) { category ->
+                                    FoodCategoryChip(
+                                        category = category.value,
+                                        onExecuteSearch = {
+                                            viewModel.onQueryChanged(it)
+                                            viewModel.newSearch(it)
+                                        }
+                                    )
+                                }
+                            })
+                        }
                     }
                     LazyColumn {
                         itemsIndexed(
