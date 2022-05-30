5b75e4c233227e7888040af3f371ee25aca3b657
Daniel Ramos <danielmfr6@gmail.com>
2022-05-30 20:44:10 -0300

    Setting navigation and testing

--- CWM/Food2ForkKmm/androidFood2ForkApp/src/main/java/br/com/daniel/ramos/food2forkkmm/android/MainActivity.kt
+++ CWM/Food2ForkKmm/androidFood2ForkApp/src/main/java/br/com/daniel/ramos/food2forkkmm/android/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.daniel.ramos.food2forkkmm.Greeting
 import android.widget.TextView
+import androidx.activity.compose.setContent
+import br.com.daniel.ramos.food2forkkmm.android.presentation.navigation.Navigation
 
 fun greet(): String {
     return Greeting().greeting()
@@ -12,9 +14,8 @@ fun greet(): String {
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        setContent {
+            Navigation()
+        }
     }
 }
