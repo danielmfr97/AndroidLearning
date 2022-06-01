7bdba959fd86d780cd60c4d3ab4bf04c2d3c23c4
Daniel Ramos <danielmfr6@gmail.com>
2022-06-01 08:25:36 -0300

    Setup hilt

--- CWM/Food2ForkKmm/androidFood2ForkApp/src/main/java/br/com/daniel/ramos/food2forkkmm/android/MainActivity.kt
+++ CWM/Food2ForkKmm/androidFood2ForkApp/src/main/java/br/com/daniel/ramos/food2forkkmm/android/MainActivity.kt
@@ -3,14 +3,15 @@ package br.com.daniel.ramos.food2forkkmm.android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.daniel.ramos.food2forkkmm.Greeting
-import android.widget.TextView
 import androidx.activity.compose.setContent
 import br.com.daniel.ramos.food2forkkmm.android.presentation.navigation.Navigation
+import dagger.hilt.android.AndroidEntryPoint
 
 fun greet(): String {
     return Greeting().greeting()
 }
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
