8b83e4cb1e0bd5d92dcb1e2e479184087972599d
Daniel Ramos <danielmfr6@gmail.com>
2022-06-01 08:20:46 -0300

    Setup navigation

--- CWM/Food2ForkKmm/androidFood2ForkApp/src/main/java/br/com/daniel/ramos/food2forkkmm/android/presentation/navigation/Navigation.kt
+++ CWM/Food2ForkKmm/androidFood2ForkApp/src/main/java/br/com/daniel/ramos/food2forkkmm/android/presentation/navigation/Navigation.kt
@@ -1,30 +1,31 @@
 package br.com.daniel.ramos.food2forkkmm.android.presentation.navigation
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.Divider
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
+import br.com.daniel.ramos.food2forkkmm.android.presentation.recipe_detail.RecipeDetailScreen
+import br.com.daniel.ramos.food2forkkmm.android.presentation.recipe_list.RecipeListScreen
 
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Screen.RecipeList.route) {
         composable(route = Screen.RecipeList.route) { navBackStackEntry ->
-            Column {
-                Text("RecipeListScreen")
-                Divider()
-                Button(onClick = { navController.navigate(Screen.RecipeDetail.route) }) {
-                    Text("Navigate")
-                }
-            }
+            RecipeListScreen(onSelectedRecipe = { recipeId ->
+                navController.navigate(Screen.RecipeDetail.route + "/$recipeId")
+            })
         }
 
-        composable(route = Screen.RecipeList.route) { navBackStackEntry ->
-            Text("RecipeDetailScreen")
+        composable(
+            route = Screen.RecipeList.route + "/{recipeId}",
+            arguments = listOf(navArgument("recipeId") {
+                type = NavType.IntType
+            })
+        ) { navBackStackEntry ->
+            RecipeDetailScreen(recipeId = navBackStackEntry.arguments?.getInt("recipeId"))
         }
     }
 }
