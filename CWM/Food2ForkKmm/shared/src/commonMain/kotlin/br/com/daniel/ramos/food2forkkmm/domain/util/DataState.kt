6f97999ad5a709d46c5f3826847994c1e339ca14
Daniel Ramos <danielmfr6@gmail.com>
2022-06-06 07:26:41 -0300

    (feat) Build a wrapper to hold our data state and emits it in flow

---
+++ CWM/Food2ForkKmm/shared/src/commonMain/kotlin/br/com/daniel/ramos/food2forkkmm/domain/util/DataState.kt
@@ -0,0 +1,26 @@
+package br.com.daniel.ramos.food2forkkmm.domain.util
+
+/*
+* Wrapper that holds data
+ */
+data class DataState<T>(
+    val message: String? = null,
+    val data: T? = null,
+    val isLoading: Boolean = false
+) {
+    // Functions that helps with states
+    companion object {
+        fun <T> error(message: String): DataState<T> {
+            return DataState(message = message)
+        }
+
+        fun <T> data(
+            message: String? = null,
+            data: T? = null,
+        ): DataState<T> {
+            return  DataState(message = message, data = data)
+        }
+
+        fun <T> loading() = DataState<T>(isLoading = true)
+    }
+}
---
+++ CWM/Food2ForkKmm/shared/src/commonMain/kotlin/br/com/daniel/ramos/food2forkkmm/interactors/recipe_list/SearchRecipes.kt
@@ -0,0 +1,27 @@
+package br.com.daniel.ramos.food2forkkmm.interactors.recipe_list
+
+import br.com.daniel.ramos.food2forkkmm.datasource.network.RecipeService
+import br.com.daniel.ramos.food2forkkmm.domain.model.Recipe
+import br.com.daniel.ramos.food2forkkmm.domain.util.DataState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class SearchRecipes(
+    private val recipeService: RecipeService,
+) {
+    fun execute(
+        page: Int,
+        query: String,
+    ): Flow<DataState<List<Recipe>>> = flow {
+        emit(DataState.loading())
+        try {
+            val recipes = recipeService.search(
+                page = page,
+                query = query
+            )
+            emit(DataState.data(message = null, data = recipes))
+        } catch (e: Exception) {
+            emit(DataState.error(message = e.message?: "Unknow error"))
+        }
+    }
+}
