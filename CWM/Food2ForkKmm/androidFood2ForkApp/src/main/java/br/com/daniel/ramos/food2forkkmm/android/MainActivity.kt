416151002f5a4905490e19bd8e93e6865dc92454
Daniel Ramos <danielmfr6@gmail.com>
2022-06-19 19:10:39 -0300

    (feat) Create recipe card

--- CWM/Food2ForkKmm/androidFood2ForkApp/src/main/java/br/com/daniel/ramos/food2forkkmm/android/MainActivity.kt
+++ CWM/Food2ForkKmm/androidFood2ForkApp/src/main/java/br/com/daniel/ramos/food2forkkmm/android/MainActivity.kt
@@ -4,30 +4,13 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import br.com.daniel.ramos.food2forkkmm.android.presentation.navigation.Navigation
-import br.com.daniel.ramos.food2forkkmm.datasource.network.KtorClientFactory
-import br.com.daniel.ramos.food2forkkmm.datasource.network.RecipeServiceImpl
-import br.com.daniel.ramos.food2forkkmm.datasource.network.RecipeServiceImpl.Companion.BASE_URL
-import br.com.daniel.ramos.food2forkkmm.domain.util.DatetimeUtil
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.launch
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val ktorClient = KtorClientFactory().build()
-        CoroutineScope(IO).launch {
-            val recipeId= 1551
-            val recipeService = RecipeServiceImpl(httpClient = ktorClient, baseUrl = BASE_URL)
-            val recipe = recipeService.get(recipeId)
-            println("KtorTesT: ${recipe.title}")
-            println("KtorTesT: ${recipe.ingredients}")
-            println("KtorTesT: ${DatetimeUtil.humanizeDatetime(recipe.dateUpdated)}")
-        }
         setContent {
             Navigation()
         }
--- CWM/Food2ForkKmm/androidFood2ForkApp/src/main/java/br/com/daniel/ramos/food2forkkmm/android/presentation/recipe_detail/RecipeDetailScreen.kt
+++ CWM/Food2ForkKmm/androidFood2ForkApp/src/main/java/br/com/daniel/ramos/food2forkkmm/android/presentation/recipe_detail/RecipeDetailScreen.kt
@@ -2,24 +2,24 @@ package br.com.daniel.ramos.food2forkkmm.android.presentation.recipe_detail
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import br.com.daniel.ramos.food2forkkmm.android.presentation.components.RecipeImage
+import br.com.daniel.ramos.food2forkkmm.android.presentation.components.RecipeCard
 import br.com.daniel.ramos.food2forkkmm.android.presentation.theme.AppTheme
 import br.com.daniel.ramos.food2forkkmm.domain.model.Recipe
 
 @Composable
 fun RecipeDetailScreen(
     recipe: Recipe?,
-){
+) {
     AppTheme(
         displayProgressBar = false
     ) {
-        if(recipe == null){
+        if (recipe == null) {
             Text("Unable to get the details of this recipe...")
-        }
-        else{
-            RecipeImage(
-                url = recipe.featuredImage,
-                contentDescription = recipe.title
+        } else {
+            RecipeCard(
+                recipe = recipe,
+                onClick = {
+                }
             )
         }
     }
